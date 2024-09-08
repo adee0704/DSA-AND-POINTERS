f52bae08f8c069617ddc9f810619a7a727674e20
Deepak Kumar <dee16108295@gmail.com>
2024-09-08 21:13:18 +0530

    commit

---
+++ Fns/thisandarrow.js
@@ -0,0 +1,14 @@
+/*const user = {
+     dee : function chai(){
+    let username ="hitesh"
+    console.log(this);
+    
+}}
+user.dee()*/
+const chai = () => {
+    let username = "deepak"
+    console.log(this);
+}
+chai()
+
+
